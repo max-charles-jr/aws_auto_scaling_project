413fce5a4ed0dc93676aa4106721ead35101d388
Max Charles <max.charles@merck.com>
2026-02-03 15:12:30 -0500

    Move files to terraform/iac

--- backend.dev.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket-dev"
-    key            = "web-app/dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-  }
-}
